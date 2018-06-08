f24f74e017a6cdbcb456a572fa0daaf6ec202eda
crea <654321su@gmail.com>
2018-06-08 13:48:48 +0900

    #027 What is the Vue Router?

---
+++ routing/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- When we request a certain page, this is where that component is loaded in. For example when we request 'HelloWorld', then it's going to get loaded in right here -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
