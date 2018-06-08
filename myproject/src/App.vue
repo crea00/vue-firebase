c520d9b671fe7e366d835a9456d968f306af72c3
crea <654321su@gmail.com>
2018-06-08 10:32:51 +0900

    #019 Nesting Components

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from "./Navbar";
+
 export default {
   name: "app",
+  components: {
+    // Navbar: Navbar in ES6
+    Navbar
+  },
   // data is function which returns an object
   // because data function protects our data
   data() {
