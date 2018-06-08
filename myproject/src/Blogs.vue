c26c40fb15d7aa94c08be9f5cdc3c1890c7a20f4
crea <654321su@gmail.com>
2018-06-08 11:29:07 +0900

    #023 Life-cycle Hooks

---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+    <button @click="changeTitle">Change Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blogs"
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.blogTitle = "Amazing Blog Site";
+    }
+  },
+  beforeCreate() {
+    alert("beforeCreate hook");
+  },
+  created() {
+    alert("created hook");
+  },
+  beforeUpdate() {
+    alert("beforeUpdate hook");
+  }
+};
+</script>
