c264325e9fb1c57c4145cad20e22f6d806b582c0
crea <654321su@gmail.com>
2018-06-08 11:00:04 +0900

    #021 Passing Data with Props

---
+++ myproject/src/AllFriends.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="all-friends">
+    <h2>All Friends</h2>
+    {{ friends }}
+    <div v-for="(friend, index) in friends" :key="index">
+      <span>{{ friend.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AllFriends",
+  // <AllFriends :friends="friends" />, props must be an array
+  // props shoud be equal :friends
+  props: ["friends"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,30 +2,35 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-     <nav>
-      <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
-      </ul>
-    </nav>  
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
 
 export default {
   name: "app",
   components: {
     // Navbar: Navbar in ES6
-    Navbar
+    Navbar,
+    AllFriends,
+    OnlineFriends
   },
   // data is function which returns an object
   // because data function protects our data
   data() {
     return {
-      title: "My first Vue app, wooo :)"
+      title: "My first Vue app, wooo :)",
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
     };
   }
 };
