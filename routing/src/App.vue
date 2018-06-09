8317dd0655a5811227e44f50ac0e9337bdb14815
crea <654321su@gmail.com>
2018-06-09 23:39:57 +0900

    #029 Router Links

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
+    <Navbar />
     <img src="./assets/logo.png">
     <!-- 
       <router-view/>
       When we request a certain page, this is where that component is loaded in. For example when we request 'HelloWorld', then it's going to get loaded in right here 
       -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Navbar
+  }
 };
 </script>
 
---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="main-nav">
+        <ul>
+            <!--
+                <li><a href="/about">About</a></li>
+                router-link is more flexible     
+            -->
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link :to="{ name: 'About' }">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+a {
+    color: #42b983;
+}
+</style>
