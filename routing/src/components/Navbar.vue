65ff8f70d555631a06767db3d9e9d3e4075a6f8d
crea <654321su@gmail.com>
2018-06-10 00:28:18 +0900

    #032 More on Router Links

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -8,6 +8,14 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link :to="{ name: 'About' }">About</router-link></li>
         </ul>
+        <h2>User Profiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+                <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">
+                    <span>Profile {{ id }}</span>
+                </router-link>
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -16,7 +24,7 @@ export default {
     name: 'Navbar',
     data() {
         return {
-
+            userIds: ['1', '2', '3', '4']
         }
     }
 }
