63f5495dd3d4aec5f5c8f60130653a8da259f183
crea <654321su@gmail.com>
2018-06-10 13:50:52 +0900

    #039 Navbar Component

---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Ninja Smoothies</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.navrbar nav {
+  padding: 0 20px;
+}
+</style>
+
+
