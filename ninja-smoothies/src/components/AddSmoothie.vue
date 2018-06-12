1c1c25d6525fff9f1bb446bb7fa64860195f39d8
crea <654321su@gmail.com>
2018-06-12 09:20:20 +0900

    #047 Add Smoothie Component

---
+++ ninja-smoothies/src/components/AddSmoothie.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <!-- 
+      If you click the button in the form, refresh and submit 
+      To prevent a default action, use .prevent and add new event
+    -->
+    <form @submit.prevent="AddSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title: </label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient:</label>
+        <input type="text" name="add-ingredient">
+      </div>
+      <div class="field center-align">
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddSmoothie',
+  data() {
+    return {
+      title: null
+    }
+  },
+  methods: {
+    AddSmoothie() {
+      console.log(this.title)
+    }
+  }
+}
+</script>
+
+<style>
+.add-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.add-smoothie .field {
+  margin: 20px auto;
+}
+</style>
--- ninja-smoothies/src/components/Navbar.vue
+++ ninja-smoothies/src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="Navbar">
     <nav class="nav-extended indigo darken-2">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{ name: 'Index' }">
           <span class="nav-title">Ninja Smoothies</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab pink">
-          <router-link to="">
+          <router-link :to="{ name: 'AddSmoothie' }">
             <i class="material-icons">add</i>
           </router-link>
         </a>
