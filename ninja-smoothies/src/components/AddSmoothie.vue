e4caf2da991cccc900aad0579b42aea19912cf3d
crea <654321su@gmail.com>
2018-06-13 19:46:36 +0900

    #048 Adding Ingredients

--- ninja-smoothies/src/components/AddSmoothie.vue
+++ ninja-smoothies/src/components/AddSmoothie.vue
@@ -12,9 +12,10 @@
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient:</label>
-        <input type="text" name="add-ingredient">
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -26,12 +27,25 @@ export default {
   name: 'AddSmoothie',
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     }
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title)
+      console.log(this.title, this.ingredients)
+    },
+    addIng() {
+      if(this.another) {
+        this.ingredients.push(this.another)
+        console.log(this.ingredients)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a value to add an ingredient'
+      }
     }
   }
 }
