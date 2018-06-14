e1e317bbcc9ad4c2d6799e29e80b6a1dcf60f78a
crea <654321su@gmail.com>
2018-06-15 08:45:01 +0900

    #054 Edit Smoothie Form

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,6 +1,25 @@
 <template>
   <div v-if="smoothie" class="edit-smoothie container">
     <h2>Edit a {{ smoothie.title }} Smoothie</h2>
+    <form @submit.prevent="EditSmoothie">
+    <div class="field title">
+      <label for="title">Smoothie Title: </label>
+      <input type="text" name="title" v-model="smoothie.title">
+    </div>
+    <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+      <label for="ingredient">Ingredient:</label>
+      <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+      <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+    </div>
+    <div class="field add-ingredient">
+      <label for="add-ingredient">Add an ingredient:</label>
+      <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+    </div>
+    <div class="field center-align">
+      <p v-if="feedback" class="red-text">{{ feedback }}</p>
+      <button class="btn pink">Update Smoothie</button>
+    </div>
+  </form>
   </div>
 </template>
 
@@ -12,6 +31,27 @@ export default {
   data() {
     return {
       smoothie: null,
+      another: null,
+      feedback: null
+    }
+  },
+  methods: {
+    EditSmoothie() {
+      console.log(this.smoothie.title, this.smoothie.ingredients)
+    },
+    addIng() {
+      if(this.another) {
+        this.smoothie.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a value to add an ingredient'
+      }
+    },
+    deleteIng(ing) {
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(ingredient => {
+        return ingredient != ing 
+      })
     }
   },
   created() {
@@ -27,5 +67,25 @@ export default {
 </script>
 
 <style>
-
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.edit-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.edit-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+.edit-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
 </style>
