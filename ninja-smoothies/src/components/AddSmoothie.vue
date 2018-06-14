0d49208ec720552069946fbaf1e822b5806b89fb
crea <654321su@gmail.com>
2018-06-14 09:10:07 +0900

    #051 Deleting Ingredients

--- ninja-smoothies/src/components/AddSmoothie.vue
+++ ninja-smoothies/src/components/AddSmoothie.vue
@@ -10,9 +10,10 @@
         <label for="title">Smoothie Title: </label>
         <input type="text" name="title" v-model="title">
       </div>
-      <div v-for="(ing, index) in ingredients" :key="index">
+      <div v-for="(ing, index) in ingredients" :key="index" class="field">
         <label for="ingredient">Ingredient:</label>
         <input type="text" name="ingredient" v-model="ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient:</label>
@@ -73,6 +74,11 @@ export default {
       } else {
         this.feedback = 'You must enter a value to add an ingredient'
       }
+    },
+    deleteIng(ing) {
+      this.ingredients = this.ingredients.filter(ingredient => {
+        return ingredient != ing 
+      })
     }
   }
 }
@@ -90,5 +96,14 @@ export default {
 }
 .add-smoothie .field {
   margin: 20px auto;
+  position: relative;
+}
+.add-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
 }
 </style>
