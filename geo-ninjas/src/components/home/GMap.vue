31ac70afbea613aad77a1dcb5e39744a201283d6
crea <654321su@gmail.com>
2018-06-17 01:57:07 +0900

    #076 Map Component

---
+++ geo-ninjas/src/components/home/GMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GMap',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
