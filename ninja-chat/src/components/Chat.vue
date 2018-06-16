0766ad0f6ec513a0b1e5c96ded5d06b8a8ade7ef
crea <654321su@gmail.com>
2018-06-16 22:20:40 +0900

    #068 Formatting Times with Moment

--- ninja-chat/src/components/Chat.vue
+++ ninja-chat/src/components/Chat.vue
@@ -21,6 +21,7 @@
 <script>
 import NewMessage from '@/components/NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
   name: 'Chat',
@@ -44,7 +45,7 @@ export default {
             id: doc.id, 
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
@@ -63,6 +64,6 @@ export default {
 }
 .chat .time {
   display: block;
-  font-size: 1.2em;
+  font-size: 0.8em;
 }
 </style>
