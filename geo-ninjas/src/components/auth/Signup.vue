2e95b1c630652e846a37aea8ce171a09b805b7e1
crea <654321su@gmail.com>
2018-06-17 23:21:40 +0900

    #081 Checking if an Alias exists

--- geo-ninjas/src/components/auth/Signup.vue
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -14,6 +14,7 @@
 				<label for="alias">Alias: </label>
 				<input type="text" name="alias" v-model="alias">
 			</div>
+			<p class="red-text center" v-if="feedback">{{ feedback }}</p>
 			<div class="field center">
 				<button class="btn deep-purple">Signup</button>
 			</div>
@@ -22,18 +23,41 @@
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+
 export default {
 	name: 'Signup',
 	data() {
 		return {
 			email: null,
-			passworld: null,
-			alias: null
+			password: null,
+			alias: null,
+			feedback: null,
+			slug: null
 		}
 	},
 	methods: {
 		signup() {
-			
+			if(this.alias) {
+				 this.slug = slugify(this.alias, {
+					 replacement: '-',
+					 remove: '',
+					 remove: /[$*_+~.()'"!\-:@]/g,
+					 lower: true
+				 })
+				 let ref = db.collection('users').doc(this.slug)
+				 ref.get().then(doc => {
+					 if(doc.exists) {
+						 this.feedback = 'This alias already exists'
+					 } else {
+						 this.feedback = 'This alias is free to use'
+					 }
+				 })
+				 console.log(this.slug)
+			} else {
+				this.feedback = "You must enter an alias"
+			}
 		}
 	}
 }
