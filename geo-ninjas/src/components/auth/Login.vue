18dc875ef809510a4f6121b23b36fb6b08d89f3c
crea <654321su@gmail.com>
2018-06-18 08:54:15 +0900

    #086 Login Component

---
+++ geo-ninjas/src/components/auth/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login container">
+    <form @submit.prevent="login" class="card-panel">
+      <h2 class="center deep-purple-text">Login</h2>
+      <div class="field">
+        <label for="email">Email: </label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password: </label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+      <div class="field">
+        <button class="btn deep-purple">Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: null,
+      password: null,
+      feedback: null
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password)
+    }
+  }
+}
+</script>
+
+<style>
+.login {
+  max-width: 400px;
+  margin-top: 60px;
+}
+.login h2 {
+  font-size: 2.4em;
+}
+.login .field {
+  margin-bottom: 16px;
+}
+</style>
--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/signup'
+import Login from '@/components/auth/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
     	path: '/signup',
     	name: 'Signup',
     	component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
