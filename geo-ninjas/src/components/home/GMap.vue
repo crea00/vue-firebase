61e66d52d39d3a2f0939b77351e22277b9e59f69
crea <654321su@gmail.com>
2018-06-17 16:18:48 +0900

    #078 Creating a New Map

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div class="google-map" id="map"></div>
   </div>
 </template>
 
@@ -9,12 +9,36 @@ export default {
   name: 'GMap',
   data() {
     return {
-
+      lat: 53,
+      lng: -2
     }
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: this.lat , lng:this.lng },
+        zoom: 6,
+        maxZoom: 15, 
+        minZoom: 3,
+        streetViewControl: false
+      })
+    }
+  },
+  mounted() {
+    this.renderMap()
   }
 }
 </script>
 
 <style>
-
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
