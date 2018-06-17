fe225209b7ae23886533a6f702e58db2d5386d63
crea <654321su@gmail.com>
2018-06-17 23:40:53 +0900

    #082 Signing up a User

--- geo-ninjas/src/components/auth/Signup.vue
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -25,6 +25,7 @@
 <script>
 import slugify from 'slugify'
 import db from '@/firebase/init'
+import firebase from 'firebase'
 
 export default {
 	name: 'Signup',
@@ -39,7 +40,7 @@ export default {
 	},
 	methods: {
 		signup() {
-			if(this.alias) {
+			if(this.alias && this.email && this.password) {
 				 this.slug = slugify(this.alias, {
 					 replacement: '-',
 					 remove: '',
@@ -51,12 +52,17 @@ export default {
 					 if(doc.exists) {
 						 this.feedback = 'This alias already exists'
 					 } else {
+						 firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+						 .catch(err => {
+							 console.log(err)
+							 this.feedback = err.message
+						 })
 						 this.feedback = 'This alias is free to use'
 					 }
 				 })
 				 console.log(this.slug)
 			} else {
-				this.feedback = "You must enter an alias"
+				this.feedback = "You must enter all fields"
 			}
 		}
 	}
