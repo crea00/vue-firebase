3a4753cac3834176e23289cffe24f6791d54f838
crea <654321su@gmail.com>
2018-06-18 09:03:50 +0900

    #087 Logging Users In

--- geo-ninjas/src/components/auth/Login.vue
+++ geo-ninjas/src/components/auth/Login.vue
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'Login',
   data() {
@@ -30,7 +32,18 @@ export default {
   },
   methods: {
     login() {
-      console.log(this.email, this.password)
+      if(this.email && this.password) {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then(cred => {
+          console.log(cred.user)
+          this.$router.push({ name: 'GMap' })
+        }).catch(err => {
+          this.feedback = err.message
+        })
+        this.feedback = null
+      } else {
+        this.feedback = 'Please fill in both fields'
+      }
     }
   }
 }
--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <nav class="deep-purple darken-1">
       <div class="container">
-        <a href="" class="brand-logo left">GeoNinjas!</a>
+        <router-link :to="{ name: 'GMap' }">GeoNinjas</router-link>
         <ul class="right">
           <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
           <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
@@ -26,7 +26,7 @@ export default {
   methods: {
     logout() {
       firebase.auth().signOut().then(() => {
-        this.$router.push({ name: 'Signup' })
+        this.$router.push({ name: 'Login' })
       })
     }
   }
