5eaff66b5c57443c496a56e9877ad76aed67b7d3
crea <654321su@gmail.com>
2018-06-19 00:11:16 +0900

    #090 Route Guarding (auth)

--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -3,20 +3,23 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
     	path: '/signup',
-    	name: 'Signup',
-    	component: Signup
+    	name: 'Signup'
     },
     {
       path: '/login',
@@ -25,3 +28,22 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // check to see if route requires auth
+  if(to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check both state of user
+    let user = firebase.auth().currentUser
+    if(user) {
+      // user signed in, proceed to route
+      next()
+    } else {
+      // no user signed in, redirect to login
+      next({ name: 'Login' })
+    } 
+  } else {
+    next()
+  }
+})
+
+export default router
