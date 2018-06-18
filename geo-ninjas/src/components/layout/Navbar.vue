3133eaf1c68fdcebc45545791ac51073c8061af6
crea <654321su@gmail.com>
2018-06-19 08:58:36 +0900

    #091 Conditional Navbar Links

--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -2,11 +2,12 @@
   <div class="navbar">
     <nav class="deep-purple darken-1">
       <div class="container">
-        <router-link :to="{ name: 'GMap' }">GeoNinjas</router-link>
+         <router-link :to="{ name: 'GMap' }" class="brand-logo left">GeoNinjas!</router-link>
         <ul class="right">
-          <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
-          <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
-          <li><a @click="logout">Logout</a></li>
+          <li v-if="!user"><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
+          <li v-if="!user"><router-link :to="{ name: 'Login' }">Login</router-link></li>
+          <li v-if="user"><a>{{ user.email }}</a></li>
+          <li v-if="user"><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -20,8 +21,19 @@ export default {
   name: 'Navbar',
   data() {
     return {
-
+      user: null  
     }
+  },
+   // this method only ever runs once
+  created() {
+    // let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.user = user
+      } else {
+        this.user = null
+      }
+    })
   },
   methods: {
     logout() {
@@ -30,6 +42,7 @@ export default {
       })
     }
   }
+ 
 }
 </script>
 
