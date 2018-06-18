f3b1a9738cf55dea7bdb576f73da2be7ed10c226
crea <654321su@gmail.com>
2018-06-18 23:39:48 +0900

    #088 Geolocation API

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -26,8 +26,21 @@ export default {
     }
   },
   mounted() {
+    // get user geolocation
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.lat = pos.coords.latitude
+        this.lng = pos.coords.longitude
+        this.renderMap()
+      }, (err) => {
+        console.log(err)
+        this.renderMap()
+      }, { maximumAge: 60000, timeout: 30000 })
+    } else {
+      // position centre by default value
+      this.renderMap()
+    }
     this.renderMap()
-    console.log(firebase.auth().currentUser)
   }
 }
 </script>
