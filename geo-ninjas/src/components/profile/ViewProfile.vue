f656f92ed07587ca11ef3ed00a7cc2e77542e660
crea <654321su@gmail.com>
2018-06-20 09:06:34 +0900

    #095 Adding Comments

--- geo-ninjas/src/components/profile/ViewProfile.vue
+++ geo-ninjas/src/components/profile/ViewProfile.vue
@@ -2,26 +2,67 @@
   <div class="view-profile container">
     <div v-if="profile" class="card">
       <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
+      <ul class="comments collection">
+        <li>Comment</li>
+        <form @submit.prevent="addComment">
+          <div class="field">
+            <label for="comment">Add a comment</label>
+            <input type="text" name="comment" v-model="newComment">
+            <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+          </div>
+        </form>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 import db from '@/firebase/init'
+import firebase from 'firebase'
 
 export default {
   name: 'ViewProfile',
   data() {
     return {
-      profile: null
+      profile: null,
+      newComment: null,
+      feedback: null,
+      user: null
     }
   },
   created() {
     let ref = db.collection('users')
+
+    // get current user
+    ref.where('user_id', '==', firebase.auth().currentUser.uid).get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        this.user = doc.data(),
+        this.user.id = doc.id
+      })
+    })
+
     ref.doc(this.$route.params.id).get()
     .then(user => {
       this.profile = user.data()
     })
+  },
+  methods: {
+    addComment() {
+      if(this.newComment) {
+        this.feedback = null
+        db.collection('comments').add({
+          to: this.$route.params.id,
+          from: this.user.id,
+          content: this.newComment,
+          time: Date.now()
+        }).then(() => {
+          this.newComment = null
+        })
+      } else {
+        this.feedback = 'You must enter a comment to add it'
+      }
+    }
   }
 }
 </script>
